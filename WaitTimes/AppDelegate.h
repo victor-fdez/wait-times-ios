92252551d22ba7de906b97e4c0ce84f9774b425c
Guillermo Vargas <gavargas22@gmail.com>
2014-03-26 00:29:48 -0600

    Initial committ

---
+++ WaitTimes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WaitTimes
+//
+//  Created by Guillermo Vargas on 3/25/14.
+//  Copyright (c) 2014 V2 Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WaitTimes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WaitTimes
+//
+//  Created by Guillermo Vargas on 3/25/14.
+//  Copyright (c) 2014 V2 Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
